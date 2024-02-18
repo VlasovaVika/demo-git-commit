f64290e65400629116965217908f1c28a01c47b3
Victoria V <a5glyk@gmail.com>
2024-02-18 21:34:04 +0300

    delete Helloworld

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args){
-		System.out.println("Hello world!");
-		User user = new User();
-		user.name = "Kate";
-		user.password = "jojopk90";
-	}
-}
