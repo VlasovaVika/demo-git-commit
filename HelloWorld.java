17d57501506f1cc5a3d93f023da5b76c36a50177
Victoria V <a5glyk@gmail.com>
2024-02-18 21:23:36 +0300

    add sout

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
+		System.out.println("Hello world!");
 		User user = new User();
 		user.name = "Kate";
 		user.password = "jojopk90";
